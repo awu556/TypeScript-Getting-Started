df9646a059b7d5a623caddddb707679646070dad
awu556 <austinw556@gmail.com>
2020-04-13 15:46:22 -0400

    updates

---
+++ app/app.ts
@@ -0,0 +1,27 @@
+function startGame() {
+  // starting a new game
+
+  let playerName: string = "Audrey";
+  logPlayer(playerName);
+
+  let messagesElement = document.getElementById("messages");
+  messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
+}
+
+function logPlayer(name: string): void {
+  console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>(
+    document.getElementById(elementID)
+  );
+
+  if (inputElement.value === "") {
+    return undefined;
+  } else {
+    return inputElement.value;  
+  }
+}
+
+document.getElementById("startGame")!.addEventListener("click", startGame);
